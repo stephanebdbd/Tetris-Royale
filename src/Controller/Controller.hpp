6a103b16b360d20b5e26fcab7844b8bec5d21da1
Ismail RHABOUQI <ismail.rhabouqi@ulb.be>
2024-11-21 20:46:06 +0100

    definir les met de la class Controlleur

--- src/Controller/Controller.hpp
+++ src/Controller/Controller.hpp
@@ -1,2 +1,19 @@
 #include <iostream>
-#include <string>
+
+#include "../Model/Tetrimino.hpp"
+#include "../Model/Grid.hpp"
+
+enum class Direction {Right, Left, Down};
+
+class Controller{
+    Tetrimino *tetramino_;
+    Grid *grid_;
+
+public:
+    Controller(Tetrimino *Tetrimino, Grid *Grid);
+    void move(Direction direction);
+    void rotate(bool clockWise);
+    bool canMove(Direction direction);
+    void keyboardInput(int keyInput);
+    ~Controller() = default;
+};
