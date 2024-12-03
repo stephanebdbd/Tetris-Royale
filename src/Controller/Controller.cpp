a2b58f0b1f079138fe9eb4272b7d95da7f4f6d34
RHABOUQI Ismail <irha0001@di-pc91.padi.addi.ulb.ac.be>
2024-12-03 12:50:35 +0100

    une petite modification dans le fichier controlleur.cpp

--- src/Controller/Controller.cpp
+++ src/Controller/Controller.cpp
@@ -1,52 +1,37 @@
-#include "Controller.hpp"
-#include <cstdlib>  
-#include <ctime>    
+#include "Controller.hpp"   
 
 
-Controller::Controller(Grid* grid, ALLEGRO_EVENT_QUEUE* queue, ALLEGRO_TIMER* timer)
-    : grid_(grid), eventQueue_(queue) {}
+Controller::Controller(Grid* grid)
+    : grid_(grid) {}
 
 Controller::~Controller() {
-    
+    //pass
 }
 
-
-// Méthode pour gérer les événements (entrées clavier)
-void Controller::handleEvents() {
-    ALLEGRO_EVENT event;
-    while (al_get_next_event(eventQueue_, &event)) {
-        switch (event.type) {
-            case ALLEGRO_EVENT_KEY_DOWN: {
-                processKeyInput(event.keyboard.keycode);
-                break;
-            }
-            
-            default:
-                break;
-        }
-    }
+char Controller::getKeyInput(){
+    std::cin >> key;
+    while (key != 'q' && key != 's' && key != 'd');
+    return key;
 }
+
 
 // Fonction pour traiter les entrées clavier et réagir en conséquence
-void Controller::processKeyInput(int keyCode) {
+void Controller::processKeyInput(char keyInput) {
     switch (keyCode) {
-        case ALLEGRO_KEY_LEFT: 
+        case 'q': 
             grid_->moveTetrimino(Direction::LEFT);  
             break;
-        case ALLEGRO_KEY_RIGHT:
+        case 'd':
             grid_->moveTetrimino(Direction::RIGHT);  
             break;
-        case ALLEGRO_KEY_DOWN:
+        case 's':
             grid_->moveTetrimino(Direction::DOWN);  
-            break;
-        case ALLEGRO_KEY_UP: 
-            grid_->rotateTetrimino();  
-            break;
-        case ALLEGRO_KEY_Q:
-            //ici il faut qu'on gere le quitte du jeu apres dans gamer 
             break;
         default:
             break;
     }
 }
+
+int mai
+
 
