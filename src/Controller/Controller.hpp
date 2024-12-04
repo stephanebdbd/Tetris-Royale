5fcaad3b547043ad26d3cece19bb3498d71ee0e3
Ismail RHABOUQI <ismail.rhabouqi@ulb.be>
2024-12-04 09:55:43 +0100

    Update Controller.hpp

--- src/Controller/Controller.hpp
+++ src/Controller/Controller.hpp
@@ -2,15 +2,17 @@
 
 #include "../include.hpp"
 #include "../Model/Grid.hpp"
-#include "../Model/Tetrimino.hpp"
+#include <allegro5/allegro.h>
 
 
 class Controller {
     Grid* grid_;  // Référence à la grille de jeu
+    ALLEGRO_EVENT_QUEUE* eventQueue_; 
     
+
 public:
-    Controller(Grid* grid);
-    char *getKeyInput();// Récupère les entrées clavier
-    void processKeyInput(const char* keyInput);  // Traite les entrées clavier
-    ~Controller();  // Destructeur
+    Controller(Grid* grid, ALLEGRO_EVENT_QUEUE* queue, ALLEGRO_TIMER* timer);            //constructeur
+    void processKeyInput(int keyCode);                                                   // Traite les entrées clavier
+    void handleEvents();
+    ~Controller()                                                            = default;  // Destructeur
 };
