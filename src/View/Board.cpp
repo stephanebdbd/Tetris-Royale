9a6652eec3637b51a7ad1cc46b8f726c4311f3cc
mohamed BOUTALEB <mohamed.boutaleb2@ulb.be>
2024-12-13 23:06:17 +0100

    modification de  Board.cpp

--- src/View/Board.cpp
+++ src/View/Board.cpp
@@ -4,12 +4,12 @@
 PlayerBoard::PlayerBoard(Game *game) : game_(game){};
 
 void PlayerBoard::clear() {
-    if (std::system("clear") != 0)
-        perror("std::system(\"clear\")");
+    ::clear();
 }
 
 void PlayerBoard::display(){
     clear();
     printw("%sTetris Royale\n", space);
     game_->display();
+    refresh();
 }
