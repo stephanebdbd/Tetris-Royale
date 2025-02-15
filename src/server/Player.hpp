1a1e529324cb5ef735116238b9c5e16ca9c436ba
Rayan  Rabeh <rayan@mac.home>
2025-02-15 02:09:15 +0100

    rajout getter observer

--- src/server/Player.hpp
+++ src/server/Player.hpp
@@ -7,12 +7,14 @@ class Player {
     int id;
     std::string name;
     bool isConnected;
+    bool isObserver;
 
 public:
     Player(int id, const std::string& name, bool isObserver = false);
     int getId() const;
     std::string getName() const;
     bool getIsConnected() const;
+    bool getIsObserver() const;
     void setIsConnected(bool status);
 };
 
