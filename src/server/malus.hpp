7f69ce0b5e8570c8572aafbd7454312049bd5f10
ikram BOUTALEB <ikram.boutaleb@ulb.be>
2025-02-15 20:40:24 +0100

    Replace malus.hpp

--- src/server/malus.hpp
+++ src/server/malus.hpp
@@ -1,17 +1,23 @@
 #ifndef MALUS_HPP
 #define MALUS_HPP
+#include <vector>
 
 class Malus {
     int nbrLine;
-    std::vector<char> line(10, '#');
-    std::vector<std::array<std::array<char>>> malus;
+    std::vector<char> line;
+
+    std::vector<std::vector<char> > malus;
     
     public:
         Malus(int nbrLine);
         void createMalus();
-        std::vector<std::array<std::array<char>>> sendMalus();
+        int getnbreLine();
+        std::vector<std::vector<char> > sendMalus();
+        std::vector<std::vector<char> > getMalus();
 
-}
+
+
+};
 
 
 
