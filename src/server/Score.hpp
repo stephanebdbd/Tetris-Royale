0cd22830bcfceca2e80f45af32e074b57176420c
Toto <anthony.van.der.wilt@ulb.be>
2025-02-21 20:09:34 +0100

    Combos

--- src/server/Score.hpp
+++ src/server/Score.hpp
@@ -6,7 +6,7 @@
 using json = nlohmann::json;
 
 class Score {
-    int score;
+    int score, combo;
     int x, y; // Position oÃ¹ afficher le score
 
     public:
@@ -14,6 +14,7 @@ class Score {
         void addScore(int linesCleared);
         void display() const;
         int getScore() const;
+        int getCombo() const;
         json scoreToJson() const;
 };
 
