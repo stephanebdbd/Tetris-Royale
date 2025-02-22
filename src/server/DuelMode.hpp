f87a41d6cb3d1c31a602a9975e783a44a18e2ead
ikram BOUTALEB <ikram.boutaleb@ulb.be>
2025-02-22 02:16:21 +0100

    reorganiser le code mode classic et duel version 1 (ikram et oumaima)

---
+++ src/server/DuelMode.hpp
@@ -0,0 +1,13 @@
+#ifndef DUELMODE_HPP
+#define DUELMODE_HPP
+
+#include <iostream>
+#include "ClassicMode.hpp"
+
+
+class DuelMode : public ClassicMode{
+    public:
+        DuelMode();
+};
+
+#endif
