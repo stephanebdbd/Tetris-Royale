c0082229ee75a00305e2e8e6788365625807a2f1
ikram BOUTALEB <ikram.boutaleb@ulb.be>
2025-03-10 02:53:37 +0100

    lobby et rejoindre une partie sont complets + il faut gerer le lancement de jeu (ikram&oumaima)

--- src/server/FriendList.hpp
+++ src/server/FriendList.hpp
@@ -1,47 +1,54 @@
 #ifndef FRIENDLIST_HPP
 #define FRIENDLIST_HPP
 
-#include "../include.hpp"
-#include "User.hpp"
+#include <string>
+#include <vector>
+#include <unordered_map>
 
 class FriendList {
 private:
-    // Liste des amis enregistrés (meme on est hors ligne)
-    std::vector<std::shared_ptr<User>> friends;
-    
-    // Liste des demandes d'amis en attente
-    std::vector<std::shared_ptr<User>> friendRequests;
-    
-    // Comptage des amis et demandes d'amis
-    int friendRequestCount = 0;
-    int friendCount = 0;
+    std::unordered_map<std::string, std::vector<std::string>> friends;
+    std::unordered_map<std::string, std::vector<std::string>> requests;
+    std::unordered_map<std::string, std::vector<std::vector<std::string>>> gameInvitations;
+    std::string friendsFile ;  // Fichier pour stocker la liste des amis
+    std::string requestsFile; // Fichier pour stocker les demandes d'amis
+    std::string gameInvitationsFile;
+
+    // charggement des donnes depuis les fichier 
+    std::unordered_map<std::string, std::vector<std::string>> loadFriends();
+    void saveFriends();
+    std::unordered_map<std::string, std::vector<std::string>> loadRequests();
+    void saveRequests();
 
 public:
     // Constructeur
-    FriendList() = default;
+    FriendList();
 
-    // Getters
-    const std::vector<std::shared_ptr<User>>& getFriends() const;
-    const std::vector<std::shared_ptr<User>>& getFriendRequests() const;
+
+    void registerUser(const std::string& username);
+    void sendFriendRequest(const std::string& sender, const std::string& receiver);
+    void acceptFriendRequest(const std::string& user, const std::string& friendToAccept);
+    void rejectFriendRequest(const std::string& user, const std::string& friendToReject);
+    bool areFriends(const std::string& user1, const std::string& user2) const;
+    bool userExists(const std::string& userId) const;
+    void removeFriend(const std::string& user, const std::string& friendToRemove);
+    void removeFriendRequest(const std::string& sender, const std::string& receiver);
+    std::vector<std::string> getFriendList(const std::string& user) const;
+    std::vector<std::string> getRequestList(const std::string& user) const;
+    bool isPendingRequest(const std::string& sender, const std::string& receiver);
     
-    int getFriendRequestCount() const;
-    int getFriendCount() const;
+    void sendInvitationToFriend(const std::string& sender, const std::string& receiver, const std::string& status, const int room); 
+    void saveGameInvitations();
+    std::unordered_map<std::string, std::vector<std::vector<std::string>>> loadGameInvitations();
+    std::vector<std::vector<std::string>> getListGameRequest(const std::string& user);
 
-    // Gérer la liste des amis enregistrés
-    void addFriend(std::shared_ptr<User> friendUser);
-    void removeFriend(std::shared_ptr<User> friendUser);
 
-    // Gérer les demandes d'amis
-    void operator>>(std::shared_ptr<User> friendUser);  // Ajouter une demande d'ami
-    void operator<<(std::shared_ptr<User> friendUser);  // Accepter une demande d'ami
 
-    // Gérer la liste des amis connectés (en ligne)
-    void addOnlineFriend(std::shared_ptr<Client> friendClient);  // Ajouter un ami connecté
-    void removeOnlineFriend(std::shared_ptr<Client> friendClient);  // Retirer un ami connecté
 
-    void processClientFriendList(int clientSocket, int clientId);   
-    // Destructeur
-    ~FriendList() = default;
+
+
+
+
 };
 
 #endif // FRIENDLIST_HPP
