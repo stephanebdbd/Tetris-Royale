6986d84af8da919edb961129c04b5e7edbc87343
Valleo <dias.valentin17@gmail.com>
2025-05-09 16:23:32 +0200

    alignement

--- src/server/TetraminoDisplacement.hpp
+++ src/server/TetraminoDisplacement.hpp
@@ -28,41 +28,42 @@ class TetraminoDisplacement {
     int counterMalus2 = -1;
     bool test = false;
     int speedtest = 0;
-public:
-    TetraminoDisplacement(Grid& grid, int speed=1000);
-    void keyInputGameMenu(const std::string& action);
-    void moveCurrentPieceDown();
-    void moveCurrentPieceRight();
-    void moveCurrentPieceLeft();
-    void rotateCurrentPiece();
-    void dropCurrentPiece();
-    void update();
-    void drawPiece();
-    void setNeedToSendGame(bool needToSendGame);
-    bool getNeedToSendGame() const;
-    bool getIsGameOver() const;
-    void setGameOver();
-    void setCurrentPiece(std::array<std::array<char, 4>, 4> shape) { this->currentPiece.setCurrentShape(shape); }
-    json tetraminoToJson() const;
-    void setSpeed(int newSpeed);
-    void setSpeedBonusMalus(bool bonus) { SpeedBonusMalus = bonus; }
-    void setBlockCommand(bool block);
-    void setlightBlocked(bool block);
-    void randomPosition();
-    void random2x2MaskedBlock();
-    const std::array<std::array<char, 4>, 4>& getCurrentShape() const {
-        return currentPiece.getCurrentShape();
-    }
+
+    public:
+        TetraminoDisplacement(Grid& grid, int speed=1000);
+        void keyInputGameMenu(const std::string& action);
+        void moveCurrentPieceDown();
+        void moveCurrentPieceRight();
+        void moveCurrentPieceLeft();
+        void rotateCurrentPiece();
+        void dropCurrentPiece();
+        void update();
+        void drawPiece();
+        void setNeedToSendGame(bool needToSendGame);
+        bool getNeedToSendGame() const;
+        bool getIsGameOver() const;
+        void setGameOver();
+        void setCurrentPiece(std::array<std::array<char, 4>, 4> shape) { this->currentPiece.setCurrentShape(shape); }
+        json tetraminoToJson() const;
+        void setSpeed(int newSpeed);
+        void setSpeedBonusMalus(bool bonus) { SpeedBonusMalus = bonus; }
+        void setBlockCommand(bool block);
+        void setlightBlocked(bool block);
+        void randomPosition();
+        void random2x2MaskedBlock();
+        const std::array<std::array<char, 4>, 4>& getCurrentShape() const {
+            return currentPiece.getCurrentShape();
+        }
+        
+        void applyMiniTetraminoBonus(){
+            nextPiece.applyMiniTetraminoBonus();
+        }
     
-    void applyMiniTetraminoBonus(){
-        nextPiece.applyMiniTetraminoBonus();
-    }
-
-    void applySpeedBonusMalus(int speed);
-
-    //Malus Royal
-    void applyMalus1();
-    void setCommandIsReversed(bool malus) { commandIsReversed = malus; }
+        void applySpeedBonusMalus(int speed);
+    
+        //Malus Royal
+        void applyMalus1();
+        void setCommandIsReversed(bool malus) { commandIsReversed = malus; }
 };
 
 #endif
